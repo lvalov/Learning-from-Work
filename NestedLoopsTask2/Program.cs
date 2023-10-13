5108bee39028bc7f6c89ea1b33a865621f7958f9
lvalov <lychezar.vylov@gmail.com>
2023-10-13 20:05:34 +0300

    update

---
+++ NestedLoopsTask2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NestedLoopsTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i > 0; i--) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ NestedLoopsTask3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NestedLoopsTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i += 2) 
+            {
+             
+            }
+        }
+    }
+}
