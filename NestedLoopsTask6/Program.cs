cd7687bc24b5331287897474fdd90dff6d45557e
lvalov <lychezar.vylov@gmail.com>
2023-10-14 19:57:34 +0300

    update

---
+++ NestedLoopsTask6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace NestedLoopsTask6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int number = int.Parse(Console.ReadLine());
+
+            int rows = 1;
+            int columns = 1;
+            while (rows <= number)
+            {
+                
+                while (columns >= rows)
+                {
+                    columns++;
+                    Console.Write(rows);
+                    
+                }
+                rows++;
+
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
