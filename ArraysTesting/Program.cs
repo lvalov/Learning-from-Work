681a4e66b311c13b0220f3e81fd8e72e11701bfc
lvalov <lychezar.vylov@gmail.com>
2023-10-30 18:01:32 +0200

    update

---
+++ ArraysTesting/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] firstNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] secondNumber = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+
+for  (int index = 0; index <= firstNumbers.Length - 1; index++)
+{
+    if (firstNumbers[index] != secondNumber[index])
+    {
+        Console.WriteLine("Arrays are not identical.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Arrays are identical.");
+    }
+}
