f3956baa42126a51b0b39c624ad41b02633ce966
lvalov <lychezar.vylov@gmail.com>
2023-10-30 20:56:41 +0200

    update

---
+++ ListTesting/Program.cs
@@ -0,0 +1,14 @@
+﻿List<int> numbers = new List<int>();
+
+numbers.Add(3);
+numbers.Add(4);
+numbers.Add(5);
+numbers.Add(6);
+numbers.Add(7);
+numbers.Add(8);    
+numbers.Add(9);
+numbers.Add(10);
+
+Console.WriteLine(numbers.Count);
+
+numbers.RemoveAll(number => number < 0);
