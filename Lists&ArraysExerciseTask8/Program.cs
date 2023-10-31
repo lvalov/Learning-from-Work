f9476c350ec7e2a7559d6e444bb69f94999a1955
lvalov <lychezar.vylov@gmail.com>
2023-10-31 17:05:23 +0200

    update

---
+++ Lists&ArraysExerciseTask8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+int rotations = int.Parse(Console.ReadLine());
+List<int> finalList = new List<int>();
+
+for (int i = 1; i <= rotations; i++)
+{
+    numbers.Insert(numbers.Count, numbers[0]);
+    numbers.RemoveAt(0);
+    finalList = numbers;
+}
+Console.WriteLine(string.Join(" ", finalList));
